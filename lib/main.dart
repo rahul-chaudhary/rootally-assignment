a4a639edd356ab80abc81c7509877ce2cbf9f79e
Rahul Chaudhary <bossrc64@outlook.com>
2024-08-19 22:48:12 +0530

    Completed UI for assessment screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 import 'Screens/assessment_detail_page.dart';
 import 'Screens/home_page.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const RootallyApp());
 }
 
