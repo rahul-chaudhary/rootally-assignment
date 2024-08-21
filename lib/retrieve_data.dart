f53d3234d404ca89d68ea27374706eeb376ccad5
Rahul Chaudhary <bossrc64@outlook.com>
2024-08-21 11:07:30 +0530

    Data is being shown on the app with firestore

--- lib/retrieve_data.dart
+++ lib/retrieve_data.dart
@@ -1,11 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:developer' as logger;
 
-Future<Map<String, dynamic>> getAllAssessment() async {
+import 'package:flutter/foundation.dart';
+
+Future<List<Map<String, dynamic>>> getAllAssessment() async {
   QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('assessment').get(); // .doc('n4O4UPnpfMjyiafCPSif').collection('title')
   Object? mp = querySnapshot.docs[0].data();
   List<Map<String, dynamic>> tempList = querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
-  logger.log(name: "Retrieved data", "${tempList.first}"); //{tempList.first.entries.first.value[1]}
+  if (kDebugMode) {
+    print("Retrieved data ${tempList.first}");
+  } //{tempList.first.entries.first.value[1]}
 
-  return tempList.first;
+  return tempList;
 }
+/*
+images ->   item.entries.last.value[0].toString(),
+descriptions -> item.entries.first.value[1].toString(),
+title -> item.entries.elementAt(1).value[0].toString(),
+ */
